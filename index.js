da08e37fa03fc2acd0f90d215fdaa5a6d52c9ea1
wonjieun <wonjieunkk@gmail.com>
2019-04-27 15:05:53 +0900

    바벨이랑 노데몬이랑 해봤음! ES6문법도

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+/* function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+} */
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
 
-function home(req, res) {
+/* function home(req, res) {
   console.log(req);
   res.send('Hello from Home');
-}
+} */
+const home = (req, res) => res.send('Hello my boot');
 
-function profile(req, res) {
+/* function profile(req, res) {
   console.log('profile');
   res.send('Hello from Profile');
-}
+} */
+const profile = (req, res) => res.send('Hello from Profile');
 
 app.get('/', home);
 app.get('/profile', profile);
