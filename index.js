b27fea5727330cf8cd23402e264b34b6e67f1f14
wonjieun <wonjieunkk@gmail.com>
2019-04-27 15:39:41 +0900

    미들웨어를 만들었다.

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-// const express = require('express');
 import express from 'express';
 const app = express();
 
 const PORT = 4000;
 
-/* function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-} */
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-/* function home(req, res) {
-  console.log(req);
-  res.send('Hello from Home');
-} */
-const home = (req, res) => res.send('Hello my boot');
+const home = (req, res) => res.send('Hello my home');
 
-/* function profile(req, res) {
-  console.log('profile');
-  res.send('Hello from Profile');
-} */
 const profile = (req, res) => res.send('Hello from Profile');
 
+const middleware = (req, res, next) => {
+  console.log('middleware');
+  next();
+};
+
+/*
+ * 미들웨어 위치 중요!
+ */
+app.use(middleware);
+
+// 미들웨어들...
+
 app.get('/', home);
+
 app.get('/profile', profile);
 
 app.listen(PORT, handleListening);
