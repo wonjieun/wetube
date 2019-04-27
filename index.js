eb7bc1bac65ec83cf2f089a32d12dbe2dac61cf8
wonjieun <wonjieunkk@gmail.com>
2019-04-27 16:08:15 +0900

    미들웨어들 - helmet, morgan

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import logger from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 const app = express();
 
 const PORT = 4000;
@@ -10,19 +14,20 @@ const home = (req, res) => res.send('Hello my home');
 
 const profile = (req, res) => res.send('Hello from Profile');
 
-const middleware = (req, res, next) => {
-  console.log('middleware');
-  next();
-};
-
 /*
  * 미들웨어 위치 중요!
  */
-app.use(middleware);
+app.use(helmet()); // 보안을 위한 미들웨어
+app.use(logger('dev')); // 로깅을 위한 미들웨어
 
-// 미들웨어들...
+// response를 가로챌 수 있음!
+const middleware = (req, res, next) => {
+  res.send('not happening ... Interrupted');
+};
 
-app.get('/', home);
+// 미들웨어들 선언 ...
+
+app.get('/', middleware, home);
 
 app.get('/profile', profile);
 
