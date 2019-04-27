de85d9083327aab296c8020a58c49af56e578a87
wonjieun <wonjieunkk@gmail.com>
2019-04-27 14:46:16 +0900

    request 보내고 response 받낑

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function home(req, res) {
+  console.log(req);
+  res.send('Hello from Home');
+}
+
+function profile(req, res) {
+  console.log('profile');
+  res.send('Hello from Profile');
+}
+
+app.get('/', home);
+app.get('/profile', profile);
+
 app.listen(PORT, handleListening);
